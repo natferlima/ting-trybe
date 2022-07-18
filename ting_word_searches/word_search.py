63f62f864bc9f03a84430c3a99b06fa737d82a84
natferlima <ferlima.natalia@gmail.com>
2022-07-17 21:21:10 -0300

    feat: implementa função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    result = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index + 1})
+        if occurrences != []:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+    return result
 
 
 def search_by_word(word, instance):
